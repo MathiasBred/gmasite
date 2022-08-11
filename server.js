fbf72bd3f10618c331056d659f69fdd5a81c3fb8
Matthew Brownell <mathiasthegreat05@gmail.com>
2022-08-11 12:30:44 -0400

    server fix??

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 let app = express();
-const port = process.env.port || 1000;
+const port = process.env.PORT || 1000;
 let server = app.listen(port, listening);
 function listening(){
     console.log(`on ${port}`);
